423d38ecb2b0b9b71640ee1fbf98d1c1f11d6e6e
elvisplaza <elvis.plaza@hotmail.com>
2021-05-25 08:40:42 -0400

    changed user model to include role, first/last names

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: {
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
     type: String,
     required: true
   },
@@ -15,6 +19,10 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
+  role: {
+    type: String,
+    enum: ['admin', 'employee']
+  }
   // organization: {
   //   type: Schema.Types.ObjectId,
   //   ref: 'Organization'
